ee639719553d1f5db8fa0a61c3c48d2cb5ac0865
Marina_Pasternak <256pasternakmarina@gmail.com>
2023-03-28 01:44:48 +0300

    added basic configuration

---
+++ curency-conversesion-app/src/App.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  data() {
+    return {
+      greeting: "Welcome to my currency exchange app!",
+      currencies: ["USD", "EUR", "UAH", "GBP", "BTC", "ETH", "BNB", "XRP"],
+    };
+  },
+});
+</script>
+
+<template>
+  <div id="app">
+    <h1>{{ greeting }}</h1>
+    <p>Here's a list of currencies:</p>
+    <ul>
+      <li v-for="currency in currencies" :key="currency">{{ currency }}</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped lang="scss">
+#app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+}
+</style>
---
+++ curency-conversesion-app/webpack.config.js
@@ -0,0 +1,56 @@
+const { VueLoaderPlugin } = require('vue-loader');
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js', '.vue', '.json'],
+    alias: {
+      vue$: 'vue/dist/vue.esm.js',
+      '@': path.resolve(__dirname, 'src/'),
+    },
+  },
+  devServer: {
+    static: 'dist',
+    port: 8080,
+    hot: true,
+  },
+};
