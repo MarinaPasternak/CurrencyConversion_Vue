1c0195abca0e9a8d30f8e0754fda2b0aa5e32be0
Marina_Pasternak <256pasternakmarina@gmail.com>
2023-03-28 22:07:50 +0300

    added basic styles and form for exchange curency

--- curency-conversesion-app/src/App.vue
+++ curency-conversesion-app/src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div id="app">
-    <h1>{{ greeting }}</h1>
-    <p>Here's a list of currencies:</p>
-    <ul>
-      <li v-for="currency in currencies" :key="currency">{{ currency }}</li>
-    </ul>
     <div>
       <currency-exchange-form></currency-exchange-form>
       <current-exchange-rate></current-exchange-rate>
@@ -20,18 +15,7 @@ export default {
     CurrencyExchangeForm,
     CurrentExchangeRate,
   },
-  data() {
-    return {
-      greeting: "Welcome to my currency exchange app!",
-      currencies: ["USD", "EUR", "UAH", "GBP", "BTC", "ETH", "BNB", "XRP"],
-    };
-  },
 };
 </script>
 
-<style scoped lang="scss">
-#app {
-  font-family: Arial, sans-serif;
-  text-align: center;
-}
-</style>
+<style scoped lang="scss"></style>
--- curency-conversesion-app/src/views/CurrencyExchangeForm.vue
+++ curency-conversesion-app/src/views/CurrencyExchangeForm.vue
@@ -1,6 +1,46 @@
 <template>
-  <div>
-    <h1>Exchange</h1>
+  <div class="container mt-3">
+    <form class="currency-exchange-form">
+      <h1 class="mb-2">Currency Converter</h1>
+      <div class="column-container mb-2">
+        <label class="mb-05">Amount To Exchange</label>
+        <input type="text" class="input-amount" />
+      </div>
+      <div class="container">
+        <div class="column-container mr-3">
+          <label class="mb-05">From</label>
+          <div class="select-container">
+            <input type="text" class="input-value" />
+            <select>
+              <option>UAH</option>
+              <option>UAH</option>
+              <option>UAH</option>
+              <option>UAH</option>
+              <option>UAH</option>
+              <option>UAH</option>
+              <option>UAH</option>
+            </select>
+          </div>
+        </div>
+        <div class="column-container">
+          <label class="mb-05">To</label>
+          <div class="select-container">
+            <input type="text" class="input-value" />
+            <select>
+              <option>UAH</option>
+              <option>UAH</option>
+              <option>UAH</option>
+              <option>UAH</option>
+              <option>UAH</option>
+              <option>UAH</option>
+              <option>UAH</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <h3 class="mt-3">1,0000 USD = 73 INR</h3>
+      <button class="primary-button">Exchange</button>
+    </form>
   </div>
 </template>
 
@@ -8,4 +48,50 @@
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "../assets/scss/main.scss";
+.currency-exchange-form {
+  width: 55%;
+  height: 400px;
+  padding: 2rem;
+  background-color: $white-color;
+  border-radius: 5px;
+
+  .input-value,
+  .input-amount {
+    padding: 10px;
+    border: 1px;
+    border: 1px solid $secondary-color;
+    border-radius: 5px;
+  }
+
+  .input-amount {
+    width: 200px;
+  }
+
+  .container {
+    justify-content: flex-start;
+  }
+
+  .primary-button {
+    width: 100%;
+  }
+  .select-container {
+    .input-value {
+      margin-right: -10px;
+    }
+
+    select {
+      padding: 9px;
+      border-radius: 0.25rem;
+      border: 1px solid $primary-color;
+      background-color: $primary-color;
+      color: $white-color;
+    }
+  }
+
+  label {
+    font-size: 1.2rem;
+  }
+}
+</style>
